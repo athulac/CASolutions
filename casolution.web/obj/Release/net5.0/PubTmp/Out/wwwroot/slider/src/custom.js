9e2c6d56dfe654707999b43f690a1a3f5550e355
athulac <caathu@gmail.com>
2021-11-07 08:13:13 +0530

    add scroll bottom

--- casolution.web/obj/Release/net5.0/PubTmp/Out/wwwroot/slider/src/custom.js
+++ casolution.web/obj/Release/net5.0/PubTmp/Out/wwwroot/slider/src/custom.js
@@ -4,7 +4,7 @@ var sliding = false;
 $(document).ready(function(){
     var options = {
         slides: '.slide', // The name of a slide in the slidesContainer
-        swipe: false,    // Add possibility to Swipe > note that you have to include touchSwipe for this
+        swipe: true,    // Add possibility to Swipe > note that you have to include touchSwipe for this
         transition: "slide", // Accepts "slide" and "fade" for a slide or fade transition
         slideTracker: true, // Add a UL with list items to track the current slide
         slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
@@ -13,10 +13,15 @@ $(document).ready(function(){
         animateDuration: 1000, // Duration of an animation
         animationEasing: 'ease', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
         pauseOnHover: true, // Pause when user hovers the slide container
-        magneticSwipe: true, // This will attach the slides to the mouse's position when swiping/dragging
+        magneticSwipe: true,//($(window).width() >= 1280), // This will attach the slides to the mouse's position when swiping/dragging
         neverEnding: true // Enable this to create a 'neverending' effect.
     };
 
+    $(".box").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".box-parent").height()
+        }, 'slow');
+    });
 
     $(".slider").height($(window).height());
     $(window).on("resize", function () {
@@ -45,17 +50,21 @@ $(document).ready(function(){
 
     /**
      * Control the slider by scrolling
-     */
-    $(window).bind('mousewheel', function(event) {
-        if(!sliding){
-            if(event.originalEvent.deltaY > 0){
+     */   
+
+    $(window).bind('mousewheel', function (event) {           
+        if (!sliding) {
+            if (event.originalEvent.deltaY > 0) {
                 mainslider.nextSlide();
             }
-            else{
+            else {
                 mainslider.prevSlide();
             }
         }
     });
+    
+
+
 
     $(".slide#first").backstretch("slider/images/bg1.jpg");
     $(".slide#sec").backstretch("slider/images/bg2.jpg");
@@ -69,3 +78,5 @@ $(document).ready(function(){
         $(this).css('margin-top', -$(this).height()/2);
     });
 });
+
+
