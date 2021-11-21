031fe2d002e9229a75242afd84f01e1e296654b1
athulac <caathu@gmail.com>
2021-11-21 14:26:13 +0530

    changes whole

--- casolution.web/bin/Release/net5.0/publish/wwwroot/slider/src/custom.js
+++ casolution.web/bin/Release/net5.0/publish/wwwroot/slider/src/custom.js
@@ -4,7 +4,7 @@ var sliding = false;
 $(document).ready(function(){
     var options = {
         slides: '.slide', // The name of a slide in the slidesContainer
-        swipe: true,    // Add possibility to Swipe > note that you have to include touchSwipe for this
+        swipe: ($(window).width() >= 1024),    // Add possibility to Swipe > note that you have to include touchSwipe for this
         transition: "slide", // Accepts "slide" and "fade" for a slide or fade transition
         slideTracker: true, // Add a UL with list items to track the current slide
         slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
@@ -13,7 +13,7 @@ $(document).ready(function(){
         animateDuration: 1000, // Duration of an animation
         animationEasing: 'ease', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
         pauseOnHover: true, // Pause when user hovers the slide container
-        magneticSwipe: ($(window).width() >= 1280),//($(window).width() >= 1280), // This will attach the slides to the mouse's position when swiping/dragging
+        magneticSwipe: ($(window).width() >= 1024),//($(window).width() >= 1280), // This will attach the slides to the mouse's position when swiping/dragging
         neverEnding: true // Enable this to create a 'neverending' effect.
     };
 
